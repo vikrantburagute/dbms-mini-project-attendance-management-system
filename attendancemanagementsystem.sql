f5186a2d7807f99c5df2b2124a60915d2733d2ac
vikrantburagute <vuser1002@gmail.com>
2026-03-14 23:08:01 +0530

    added integrity constraints, more sample data

--- attendancemanagementsystem.sql
+++ attendancemanagementsystem.sql
@@ -3,23 +3,23 @@ CREATE DATABASE attendance_db;
 USE attendance_db;
 
 CREATE TABLE students (
-	student_id INT PRIMARY KEY AUTO_INCREMENT, 
-    roll_no VARCHAR(20) UNIQUE,
-    name VARCHAR(100),
-    department VARCHAR(50),
-    semester INT
+	student_id INT PRIMARY KEY AUTO_INCREMENT, /*PRIMARY KEY automatically implies NOT NULL */
+    roll_no VARCHAR(20) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    semester INT NOT NULL CHECK (semester BETWEEN 1 AND 8)
 );
 
 CREATE TABLE subjects (
 	subject_id INT PRIMARY KEY AUTO_INCREMENT,
-    subject_name VARCHAR(100),
-    subject_code VARCHAR(20)
+    subject_name VARCHAR(100) NOT NULL,
+    subject_code VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE teachers (
 	teacher_id INT PRIMARY KEY AUTO_INCREMENT,
-    teacher_name VARCHAR(100),
-    email VARCHAR(100)
+    teacher_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE attendance (
@@ -34,16 +34,22 @@ CREATE TABLE attendance (
     FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
 );
 
+ALTER TABLE attendance
+ADD CONSTRAINT unique_attendance
+UNIQUE (student_id, subject_id, date);
+
 INSERT INTO students (roll_no, name, department, semester)
 VALUES
 ('SE001', 'Rahul Sharma', 'Computer', 3),
-('SE002', 'Priiya Patel', 'Computer', 3),
+('SE002', 'Priya Patel', 'Computer', 3),
 ('SE003', 'Amit Verma', 'Computer', 3);
 
 INSERT INTO subjects (subject_name, subject_code)
 VALUES
 ('DBMS', 'CS301'),
-('Operating System', 'CS302');
+('IOT', 'CS302'),
+('CO&MP', 'CS303'),
+('DM', 'CS304');
 
 INSERT INTO teachers (teacher_name, email)
 VALUES
@@ -52,16 +58,24 @@ VALUES
 
 INSERT INTO attendance (student_id, subject_id, teacher_id, date, status)
 VALUES
-(1, 1, 1, '2026-03-01', 'Present'),
-(2, 1, 1, '2026-03-01', 'Absent'),
-(3, 1, 1, '2026-03-01', 'Present');
+(1, 1, 1, '2026-03-02', 'Present'),
+(2, 1, 1, '2026-03-02', 'Present'),
+(3, 1, 1, '2026-03-02', 'Absent'),
+(1, 2, 2, '2026-03-01', 'Absent'),
+(2, 2, 2, '2026-03-01', 'Present'),
+(3, 2, 2, '2026-03-01', 'Present'),
+(1, 2, 2, '2026-03-02', 'Present'),
+(2, 2, 2, '2026-03-02', 'Absent'),
+(3, 2, 2, '2026-03-02', 'Present');
 
 SELECT
 	s.roll_no,
     s.name,
     sub.subject_name,
+    t.teacher_name,
     a.date,
     a.status
 FROM attendance a
 JOIN students s ON a.student_id = s.student_id
-JOIN subjects sub ON a.subject_id = sub.subject_id;
+JOIN subjects sub ON a.subject_id = sub.subject_id
+JOIN teachers t ON a.teacher_id = t.teacher_id;
